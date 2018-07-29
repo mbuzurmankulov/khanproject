22bc6e6caf00b6038fca518fd1760b31a6f7cf9e
mbuzurmankulov <makhambet.b@gmail.com>
2018-07-29 17:15:43 +0600

    First commit

---
+++ src/main/java/kz/makha/khan/Main.java
@@ -0,0 +1,10 @@
+package kz.makha.khan;
+
+/**
+ * Created by Makha on 29.07.2018.
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
